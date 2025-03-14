98532ed719a7b80bd1baabb2ae48671b15993ee6
SC201-oct24groupc <sc201oct24.groupc@gmail.com>
2025-03-14 11:42:16 -0700

    Update app.py
    import main instead of recommend_food

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
-from recommendation import recommend_food
+from recommendation import main
 import tempfile, os,re
 import datetime
 import openai
