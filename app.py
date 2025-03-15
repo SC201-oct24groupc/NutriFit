c55e1866d050b3c32651e897f37c8645afd2d90b
Mirandayao0 <122713461+Mirandayao0@users.noreply.github.com>
2025-03-15 15:13:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def callback():
 
 
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
+#@handler.add(MessageEvent, message=TextMessage)
 # def handle_message(event):
 #     msg = event.message.text
 #     try:
